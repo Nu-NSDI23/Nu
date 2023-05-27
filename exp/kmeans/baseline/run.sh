730896cb74ecbdc032567f6a894d4748a28ff537
Zhenyuan Ruan <zainryan95@gmail.com>
2023-05-27 11:18:11 -0600

    exp: add a script for kmeans/baseline experiment.

---
+++ exp/kmeans/baseline/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source ../../shared.sh
+
+DIR=`pwd`
+
+cd ../../../app/phoenix++-1.0/orig/
+make clean
+make -j
+cd tests/kmeans
+
+for num_threads in `seq 1 46`
+do
+    export MR_NUMTHREADS=$num_threads
+    ./kmeans 1>$DIR/logs/$num_threads 2>&1 &
+    pid=$!
+    ( tail -f -n0 $DIR/logs/$num_threads & ) | grep -q "iter = 10"
+    kill -9 $pid
+    sleep 5
+done

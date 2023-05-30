daaabc51b616a647b2c38722a401d0d2bf5598c2
Zhenyuan Ruan <zainryan95@gmail.com>
2023-05-30 15:56:10 -0600

    socialNet: upstream's latest DNS server is buggy; force using the old one.

--- app/socialNetwork/multi_objs/up_nginx.sh
+++ app/socialNetwork/multi_objs/up_nginx.sh
@@ -4,7 +4,7 @@ docker run -d --rm --hostname dns.mageddo -p 5380:5380 \
        -v /var/run/docker.sock:/var/run/docker.sock \
        -v /etc/resolv.conf:/etc/resolv.conf \
        -v `pwd`/config/dns.json:/app/conf/config.json \
-       defreitas/dns-proxy-server
+       defreitas/dns-proxy-server:2.19.0
 
 pushd ymls
 docker-compose -f nginx-thrift.yml create

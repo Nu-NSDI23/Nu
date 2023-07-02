06fd662085608672726a30a05d66080ef48f5307
Zhenyuan Ruan <zainryan95@gmail.com>
2023-07-02 10:24:14 -0600

    caladan: improve MAKE_IP_ADDR().

--- caladan/inc/net/ip.h
+++ caladan/inc/net/ip.h
@@ -43,9 +43,9 @@
  */
 #define	IPVERSION	4
 
-#define MAKE_IP_ADDR(a, b, c, d)			\
-	(((uint32_t) a << 24) | ((uint32_t) b << 16) |	\
-	 ((uint32_t) c << 8) | (uint32_t) d)
+#define MAKE_IP_ADDR(a, b, c, d)                           \
+       (((uint32_t) (a) << 24) | ((uint32_t) (b) << 16) |  \
+        ((uint32_t) (c) << 8) | (uint32_t) (d))
 
 #define IP_ADDR_STR_LEN	16
 

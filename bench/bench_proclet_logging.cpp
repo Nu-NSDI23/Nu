9bc2bd0c0ac19c5065d8394b40009ddb84e8dd8d
bokaibi <bokaibi@localhost>
2023-07-10 12:52:57 -0600

    Updated benchmark for multithreaded proclet logging performance

--- bench/bench_proclet_logging.cpp
+++ bench/bench_proclet_logging.cpp
@@ -10,10 +10,11 @@
 using namespace nu;
 using namespace std;
 
-constexpr static uint32_t kNumThreads = 10;
+constexpr static uint32_t kNumProclets = 100;
 constexpr static uint32_t kObjSize = 100;
 constexpr static uint32_t kBufSize = 102400;
-constexpr static uint32_t kNumInvocationsPerThread = 500000;
+constexpr static uint32_t kNumInvocationsPerProclet = 10;
+constexpr static uint32_t kNumThreadsPerProclet = 500;
 
 struct Obj {
   uint8_t data[kObjSize];
@@ -40,13 +41,28 @@ class Worker {
   Worker(Proclet<TableDB> tabledb)
   : _tabledb(std::move(tabledb)){}
 
-  bool foo(uint32_t kNumThreads) {
-    for (int64_t i = 0; i < kNumThreads; i++){
-      int64_t v = _tabledb.run(&TableDB::get, i);
-      if (v != i){
-        std::cout << "BADRESULT" << v;
-      }
+  bool foo(uint32_t kNumProclets) {
+    std::vector<rt::Thread> threads;
+    threads.reserve(kNumThreadsPerProclet);
+
+    //auto start = rdtsc();
+    //uint64_t starts_tid[kNumThreadsPerProclet];
+    for (uint32_t i = 0; i < kNumThreadsPerProclet; i++) {
+      threads.emplace_back([&] {
+        for (int64_t i = 0; i < kNumProclets; i++){
+          int64_t v = _tabledb.run(&TableDB::get, i);
+          if (v != i){
+            std::cout << "BADRESULT" << v;
+            BUG_ON(true);
+          }
+        }
+      });
     }
+
+    for (auto &thread : threads) {
+      thread.Join();
+    }
+    
     return true;
   }
   private:
@@ -62,22 +78,22 @@ void do_work() {
   NodeIP remoteip = 303169794;
   auto dis_hash = make_proclet<TableDB>(true, std::nullopt, localip);
 
-  for (int64_t i = 0; i < kNumThreads; i++) {
+  for (int64_t i = 0; i < kNumProclets; i++) {
     dis_hash.run(&TableDB::put, i, i);
   }
-  for (int64_t i = 0; i < kNumThreads; i++) {
+  for (int64_t i = 0; i < kNumProclets; i++) {
     workers.emplace_back(make_proclet<Worker>(
-      std::forward_as_tuple(dis_hash), false, std::nullopt, remoteip
+      std::forward_as_tuple(dis_hash), true, std::nullopt, remoteip
     ));
   }
 
   std::vector<rt::Thread> ths;
-  for (uint32_t i = 0; i < kNumThreads; i++) {
+  for (uint32_t i = 0; i < kNumProclets; i++) {
     ths.emplace_back([worker = std::move(workers[i])]() mutable {
       //Buf buf;
       //buf.resize(kBufSize / kObjSize);
-      for (uint32_t j = 0; j < kNumInvocationsPerThread; j++) {
-        bool result = worker.run(&Worker::foo, kNumThreads);
+      for (uint32_t j = 0; j < kNumInvocationsPerProclet; j++) {
+        bool result = worker.run(&Worker::foo, kNumProclets);
         BUG_ON(!result);
         //if (!result){
           //std::cout << "Wrong output from foo" << std::endl;
@@ -94,8 +110,8 @@ void do_work() {
   auto t1 = microtime();
   auto us = t1 - t0;
   //auto size =
-  //    static_cast<uint64_t>(kBufSize) * kNumInvocationsPerThread * kNumThreads;
-  auto size = sizeof(int64_t) * kNumThreads * kNumThreads + (kNumThreads * (1 + sizeof(int64_t)));
+  //    static_cast<uint64_t>(kBufSize) * kNumInvocationsPerProclet * kNumProclets;
+  auto size = sizeof(int64_t) * kNumProclets * kNumProclets + (kNumProclets * (1 + sizeof(int64_t)));
   auto mbs = size / us;
 
 
